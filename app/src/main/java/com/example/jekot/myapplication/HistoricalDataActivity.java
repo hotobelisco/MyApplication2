a0975d1d52446331a738afb022462b73a5e8a6d7
Kotasek94 <josekotasek@gmail.com>
2016-09-27 16:36:37 -0500

    Navegación y Gráficas Implementadas Preeeliminar

---
+++ app/src/main/java/com/example/jekot/myapplication/HistoricalDataActivity.java
@@ -0,0 +1,76 @@
+package com.example.jekot.myapplication;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class HistoricalDataActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_historical_data, container, false);
+        super.onCreateView(inflater, container,savedInstanceState);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initializeLineChart();
+
+    }
+
+    protected void initializeLineChart(){
+        
+        LineChart lineChart = (LineChart) this.getView().findViewById(R.id.line_chart_historical);
+        //System.out.println( != null);
+
+        LineDataSet lineDataSet = new LineDataSet(getData(),"Algo VS Algo");
+
+        LineData lineData = new LineData(lineDataSet);
+
+        lineChart.setData(lineData);
+
+        lineChart.invalidate();
+    }
+
+    protected ArrayList<Entry> getData(){
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(4f, 0));
+        entries.add(new Entry(6f, 1));
+        entries.add(new Entry(8f, 2));
+        entries.add(new Entry(5f, 3));
+        entries.add(new Entry(7f, 4));
+        entries.add(new Entry(9f, 5));
+
+
+        return entries;
+    }
+
+    protected ArrayList<String> getLabels(){
+        ArrayList<String> labels = new ArrayList<>();
+        labels.add("Enero");
+        labels.add("Enero");
+        labels.add("Ener1");
+        labels.add("Enero");
+        labels.add("Enero");
+        labels.add("Ener3");
+
+        return labels;
+    }
+
+}
---
+++ app/src/main/java/com/example/jekot/myapplication/NavigationDrawerActivity.java
@@ -0,0 +1,131 @@
+package com.example.jekot.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NavigationDrawerActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    /*
+        PieChart chart = (PieChart) findViewById(R.id.chart_2);
+
+        List<PieEntry> arr = new ArrayList<>();
+        arr.add(new PieEntry(40.0f, "Pato"));
+        arr.add(new PieEntry(35.0f, "Pollo"));
+        arr.add(new PieEntry(25.0f, "Puerco"));
+
+        PieDataSet ps = new PieDataSet(arr, "Saludos, jaja");
+        ps.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        ps.setValueTextSize(25f);
+        PieData pd = new PieData(ps);
+        chart.setDescription("");
+        chart.setData(pd);
+        chart.invalidate();
+        */
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.navigation_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        Fragment frag = null;
+        if (id == R.id.nav_dashboard) {
+            System.out.println("asdasd");
+            frag = new HistoricalDataActivity();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content_navigation_drawer,frag)
+                    .commit();
+        } else if(id == R.id.nav_por_fecha){
+
+        } else if(id == R.id.nav_about){
+            System.out.println("About");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
